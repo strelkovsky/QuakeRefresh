0b0f997ef1c27170a1e823dbcb594fd770e8acd3
strelkovsky <strelkin7@gmail.com>
2016-12-05 12:58:07 +0300

    remove QUAKE2 define

--- WinQuake/cl_input.c
+++ WinQuake/cl_input.c
@@ -324,12 +324,7 @@ void CL_BaseMove (usercmd_t *cmd)
 		cmd->sidemove *= cl_movespeedkey.value;
 		cmd->upmove *= cl_movespeedkey.value;
 	}
-
-#ifdef QUAKE2
-	cmd->lightlevel = cl.light_level;
-#endif
 }
-
 
 
 /*
@@ -381,13 +376,6 @@ void CL_SendMove (usercmd_t *cmd)
 
     MSG_WriteByte (&buf, in_impulse);
 	in_impulse = 0;
-
-#ifdef QUAKE2
-//
-// light level
-//
-	MSG_WriteByte (&buf, cmd->lightlevel);
-#endif
 
 //
 // deliver the message
